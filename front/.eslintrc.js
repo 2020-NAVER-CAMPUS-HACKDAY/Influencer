42b6989872e5fa95bfa66fc16c0e10ac62305707
jeongdaeun <daeun@squarelab.co>
2020-05-07 13:20:12 +0900

    Set Next.js and fit ESLint with Next.js

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['airbnb-typescript'],
+  extends: [
+    'airbnb-typescript/base',
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
-}
+};
