39844740e043179ea14e09385f4b79b20196664b
jominjimail <jominjimail@gmail.com>
2020-05-09 02:54:50 +0900

    Add getLocale function in utils
    for html lang tag
    : https://www.w3schools.com/tags/ref_language_codes.asp

---
+++ client/utils/getLocale.tsx
@@ -0,0 +1,29 @@
+const DEFAULT_LOCALE = 'ko-KR';
+const AVAILABLE_LOCALES = ['en-US', 'ko-KR'];
+const FALLBACK_LOCALES_PER_LANGUAGE: Record<string, string> = {
+  en: 'en-US',
+  ko: 'ko-KR',
+};
+
+function sanitizeLocale(value?: string): string {
+  if (value) {
+    if (AVAILABLE_LOCALES.includes(value)) {
+      return value;
+    }
+
+    const language = value.split('-')[0];
+    const locale = FALLBACK_LOCALES_PER_LANGUAGE[language];
+
+    if (locale && AVAILABLE_LOCALES.includes(locale)) {
+      return locale;
+    }
+  }
+
+  return DEFAULT_LOCALE;
+}
+
+function getLocale(query: any) {
+  return sanitizeLocale(query.hl);
+}
+
+export default getLocale;
