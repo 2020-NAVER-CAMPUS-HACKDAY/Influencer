f2b7d412b05ff7aaf6f2c84235aee635fafbee2b
jeongdaeun <daeun@squarelab.co>
2020-05-09 17:06:13 +0900

    add base code to use store

--- client/views/defaultView/index.tsx
+++ client/views/defaultView/index.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import useStyles from 'views/defaultView';
+import useStyles from 'views/defaultView/styles';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { UserProps, UserMethods, AuthActions } from 'redux/ducks/auth';
 
-interface DefaultProps {
+interface DefaultProps extends UserProps, UserMethods {
   data: string;
 }
 
+const userData = { id: 'dgsda', thumbnail: 'dgsadg' };
+
 const Home: React.FC<DefaultProps> = (props) => {
-  const { data } = props;
+  const { data, setUser, user } = props;
   const classes = useStyles(props);
+  const setUserData = (): void => setUser(userData);
 
   return (
     <>
       <Head>
         <title>Influencer</title>
       </Head>
-      <div className={classes.root}>
+      <div className={classes.root} onClick={setUserData}>
         This is Default Page.
       </div>
       <Link href="/routeExample/example1">
@@ -27,4 +33,11 @@ const Home: React.FC<DefaultProps> = (props) => {
   );
 };
 
-export default Home;
+export default connect<UserProps, UserMethods, void>(
+  (state: UserProps) => ({
+    user: state.user,
+  }),
+  (dispatch) => ({
+    setUser: bindActionCreators(AuthActions.setUser, dispatch),
+  }),
+)(Home);
