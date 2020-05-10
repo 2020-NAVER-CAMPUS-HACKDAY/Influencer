d397c5416b4afef073b68c3031065889e0afa403
jominjimail <jominjimail@gmail.com>
2020-05-10 23:07:41 +0900

    Fix Unexpected tab character issue

--- client/components/AppBar/index.tsx
+++ client/components/AppBar/index.tsx
@@ -8,18 +8,18 @@ const AppBar: React.FunctionComponent = (props) => {
   const classes = useStyles(props);
 
   return (
-		<MaterialAppBar className={classes.root}>
-			<div className={classes.container}>
-				<div className={classes.logo}>
-					INFLUENCER
-				</div>
-				<div>
-					<button className={classes.button}>
-						<HamburgerMenu />
-					</button>
-				</div>
-			</div>
-		</MaterialAppBar>
+  <MaterialAppBar className={classes.root}>
+    <div className={classes.container}>
+      <div className={classes.logo}>
+        INFLUENCER
+      </div>
+      <div>
+        <button className={classes.button}>
+          <HamburgerMenu />
+        </button>
+      </div>
+    </div>
+  </MaterialAppBar>
   );
 };
 
