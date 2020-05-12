cc179b2322685a440d68d791c2ab7510af8a0966
daeun <daeun@squarelab.co>
2020-05-12 12:20:23 +0900

    Modify components to make it more global

--- client/constant.ts
+++ client/constant.ts
@@ -3,6 +3,8 @@ export const AppColor = {
   WHITE: '#FFFFFF',
   GREEN: '#01C73B',
   PURPLE: '#827EDB',
+  MAIN_HEADER: 'linear-gradient(to right, #01C73B, #827EDB)',
+  DETAIL_HEADER: '#8452C7',
 };
 
 export const CONSTANT_TAG = 24;
--- client/svgs/HamburgerMenu/index.tsx
+++ client/svgs/HamburgerMenu/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-const SvgComponent = () => (
+const SvgComponent = (): ReactElement => (
   <svg width={24} height={24} viewBox="0 0 100 80">
     <rect width="100" height="13" fill="white"/>
     <rect y="30" width="100" height="13" fill="white"/>
