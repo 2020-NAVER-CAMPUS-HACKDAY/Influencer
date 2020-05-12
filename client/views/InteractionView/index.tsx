80c8990b80443cced5216497d6be2c2a17eef372
Seogeurim <geulims@naver.com>
2020-05-13 05:23:02 +0900

    Add Swiper Component skeleton code based on react-hammerjs

--- client/views/InteractionView/index.tsx
+++ client/views/InteractionView/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import Layout from 'components/Layout';
+import SwiperCard from 'components/Swiper/Card';
 
 const Interaction: React.FC = (props) => {
-  const { children } = props;
+  // const { children } = props;
   return (
     <Layout>
       <div>
         Interaction Page.
       </div>
+      <SwiperCard />
     </Layout>
   );
 };
