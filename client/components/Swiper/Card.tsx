ab4d5725991937a052b1a9880b44c1b05a154656
Seogeurim <geulims@naver.com>
2020-05-15 02:14:23 +0900

    Modify Card Animation using State

--- client/components/Swiper/Card.tsx
+++ client/components/Swiper/Card.tsx
@@ -1,76 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useStyles from 'components/Swiper/styles';
 import Hammer from 'react-hammerjs';
-
-// export interface CardProps {
-//   // xMulti?: number;
-//   // yMulti?: number;
-//   // rotate?: number;
-// }
 
 const Card: React.FC = (props) => {
   const { children } = props;
   const classes = useStyles(props);
-
-  useEffect(() => {
-    // componentDidMount
-  }, []);
+  const [offset, setOffset] = useState([0, 0]);
+  const [rotation, setRotation] = useState(0);
+  const [isMoving, setIsMoving] = useState(false);
 
   function handlePan(event) {
-    let xMulti = 0;
-    let yMulti = 0;
-    let rotate = 0;
     if (event.deltaX <= 0 || event.deltaX >= 0) {
-      if (event.deltaX === 0) return;
-      if (event.center.x === 0 && event.center.y === 0) return;
-      xMulti = event.deltaX * 0.03;
-      yMulti = event.deltaY / 80;
-      rotate = xMulti * yMulti;
-      event.target.style.transform = `translate(${event.deltaX}px, ${event.deltaY}px) rotate(${rotate}deg)`;
+      setIsMoving(true);
+      setOffset([event.deltaX, event.deltaY]);
+      setRotation((event.deltaX * 0.03) * (event.deltaY / 80));
     }
   }
 
   function handlePanEnd(event) {
-    // 잡기 끝
     let moveOutWidth = 0;
     let keep = false;
-    let endX = 0;
-    let toX = 0;
-    let endY = 0;
-    let toY = 0;
-    let xMulti = 0;
-    let yMulti = 0;
-    let rotate = 0;
-    console.log('onPanEnd');
-    if (event.deltaX <= 0 || event.deltaX >= 0){
-      moveOutWidth = document.body.clientWidth;
+    let [endX, toX, endY, toY] = [0, 0, 0, 0];
+    setIsMoving(false);
+    if (event.deltaX <= 0 || event.deltaX >= 0) {
       keep = Math.abs(event.deltaX) < 300;
       if (keep) {
         event.target.style.transform = '';
       } else {
-        endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
+        moveOutWidth = document.body.clientWidth;
+        endX = event.velocityX > 1 ? Math.abs(event.velocityX) * moveOutWidth : moveOutWidth;
         toX = event.deltaX > 0 ? endX : -endX;
         endY = Math.abs(event.velocityY) * moveOutWidth;
         toY = event.deltaY > 0 ? endY : -endY;
-        xMulti = event.deltaX * 0.03;
-        yMulti = event.deltaY / 80;
-        rotate = xMulti * yMulti;
-        event.target.style.transform = `translate(${toX}px, ${toY + event.deltaY}px) rotate(${rotate}deg)`;
-        // DO SWIPE ACTIONS
-        // this.props.superOnSwipe();
-        // if(this.props.onSwipe) this.props.onSwipe(this.props.data);
-        // if(toX < 0 && this.props.onSwipeLeft) {
-        //   this.props.onSwipeLeft(this.props.data);
-        // } else if(this.props.onSwipeRight) {
-        //   this.props.onSwipeRight(this.props.data);
-        // }
+        setOffset([toX, toY + event.deltaY]);
+        setRotation((event.deltaX * 0.03) * (event.deltaY / 80));
       }
     }
   }
 
-  function handleDoubleTap() {
-    // 찜하기 + 위로 보내기
+  function handleDoubleTap(event) {
     alert('You Liked this Product!');
+    setOffset([0, -(document.body.clientWidth * 0.3) - 100]);
+    setRotation(80);
   }
 
   return (
@@ -79,7 +50,12 @@ const Card: React.FC = (props) => {
       onPanEnd={handlePanEnd}
       onDoubleTap={handleDoubleTap}
     >
-      <div className={classes.card}>
+      <div
+        className={classes.card}
+        style={{
+          cursor: (isMoving ? 'grabbing' : 'grab'),
+          transform: `translate(${offset[0]}px, ${offset[1]}px) rotate(${rotation}deg)`,
+        }}>
         {children}
       </div>
     </Hammer>
