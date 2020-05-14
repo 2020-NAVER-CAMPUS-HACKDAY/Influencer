9a5dadce9dc21f92d3734e3f4f9dc367e21e9b73
Seogeurim <geulims@naver.com>
2020-05-14 23:16:38 +0900

    Add Swiper index and Modify Card & CardWrapper name

--- client/components/Swiper/index.tsx
+++ client/components/Swiper/index.tsx
@@ -1,4 +1,25 @@
-import SwiperCard from 'components/Swiper/Card';
-import SwiperCardWrapper from 'components/Swiper/CardWrapper';
+import React from 'react';
+import Card from 'components/Swiper/Card';
+import CardWrapper from 'components/Swiper/CardWrapper';
 
-export default { SwiperCard, SwiperCardWrapper };
+const Swiper: React.FC = (props) => {
+  const { children } = props;
+
+  function renderCards() {
+    const cardInfo = ['First', 'Second', 'Third', 'Fourth'];
+    return cardInfo.map((data, index) => (
+      <Card
+        key={index}>
+        {data} Hello World!
+      </Card>
+    ));
+  }
+
+  return (
+    <CardWrapper>
+      {renderCards()}
+    </CardWrapper>
+  );
+};
+
+export default Swiper;
