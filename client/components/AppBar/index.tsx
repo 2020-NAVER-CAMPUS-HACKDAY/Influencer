0db718e9fa109a88aa08d6e49b40feac04f2ce46
daeun <daeun@squarelab.co>
2020-05-15 13:23:39 +0900

    add ClassName to mix style

--- client/components/AppBar/index.tsx
+++ client/components/AppBar/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import useStyles from 'components/AppBar/styles';
 import MaterialAppBar from '@material-ui/core/AppBar';
+import clsx from 'clsx';
 
 export interface BarProps {
   isNotFixed?: boolean;
   backgroundColor?: string;
   backgroundImage?: string;
   height?: string;
+  className?: string;
 }
 
 const AppBar: React.FunctionComponent<BarProps> = (props) => {
   const classes = useStyles(props);
-  const { children } = props;
+  const { children, className } = props;
 
   return (
   <MaterialAppBar className={classes.root}>
-    <div className={classes.container}>
+    <div className={clsx(classes.container, className)}>
       {children}
     </div>
   </MaterialAppBar>
