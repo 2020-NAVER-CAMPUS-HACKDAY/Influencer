4c1c0c5364da1f463163b5dd8e61f6ae04ac33a4
jeongdaeun <daeun@squarelab.co>
2020-05-17 01:22:58 +0900

    Fix Lint error

--- client/views/myCategoryView/index.tsx
+++ client/views/myCategoryView/index.tsx
@@ -23,7 +23,7 @@ const MyCategory: React.FC<DefaultProps> = (props) => {
   const [categories, setCategories] = useState<Category[]>([]);
   const dummyData: Category[] = myCategoryViewDataArray;
 
-  const categoryAddHandler = (id: string, name: string) => {
+  const categoryAddHandler = (id: string, name: string): void => {
     setCategories([
       ...categories,
       {
@@ -33,7 +33,7 @@ const MyCategory: React.FC<DefaultProps> = (props) => {
     ]);
   };
 
-  const categoryDeleteHandler = (id: string) => {
+  const categoryDeleteHandler = (id: string): void => {
     const newCategories = categories.filter((category) => category.id !== id);
     setCategories(newCategories);
   };
