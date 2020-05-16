9f507b1f3eac6c0216db15d2a4ea5a52ce8c3262
jeongdaeun <daeun@squarelab.co>
2020-05-16 14:47:26 +0900

    Add likeListBar Component

---
+++ client/components/LikeList/LikeListBar/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import useStyles from 'components/LikeList/LikeListBar/styles';
+import AppBar from 'components/Common/AppBar';
+import SVGButton from 'components/Common/SVGButton';
+import ClickedGrid from 'svgs/ClickedGrid';
+import ClickedList from 'svgs/ClickedList';
+import UnclickedGrid from 'svgs/UnclickedGrid';
+import UnclickedList from 'svgs/UnclickedList';
+import { AppColor } from 'constant';
+import Label from 'components/Common/Label';
+import LikeListBarProps from 'components/LikeList/LikeListBar/interface';
+
+const LikeListBar: React.FC<LikeListBarProps> = (props) => {
+  const classes = useStyles();
+  const {
+    listClicked,
+    handleGridClicked,
+    gridClicked,
+    handleListClicked,
+  } = props;
+
+  return (
+    <div className={classes.marginBottom}>
+      <AppBar height={'3.7rem'} backgroundColor={AppColor.WHITE} isNotFixed>
+        <Label name={'찜한 상품'} color={AppColor.BLACK} fontSize={20}/>
+        <div className={classes.rightPosition}>
+          <SVGButton handleClick={handleListClicked}>
+            { listClicked ? <ClickedList /> : <UnclickedList /> }
+          </SVGButton>
+          <SVGButton handleClick={handleGridClicked}>
+            { gridClicked ? <ClickedGrid /> : <UnclickedGrid /> }
+          </SVGButton>
+        </div>
+      </AppBar>
+    </div>
+  );
+};
+
+export default LikeListBar;
