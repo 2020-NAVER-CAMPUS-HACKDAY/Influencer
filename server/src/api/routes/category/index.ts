3a48c373c17d49b37db16a147d9d78c746af50c3
jominjimail <jominjimail@gmail.com>
2020-05-16 17:41:27 +0900

    Add get category By categoryLevel

--- server/src/api/routes/category/index.ts
+++ server/src/api/routes/category/index.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { Container } from 'typedi';
 import winston from 'winston';
 import CategoryService from '../../../services/category';
+import ProductService from "../../../services/product";
 
 const categoryRoute = Router();
 
@@ -18,8 +19,28 @@ export default (routes: Router) => {
         const categoryServiceInstance = Container.get(CategoryService);
         const { categories } = await categoryServiceInstance.list(
           req.query.limit as string,
+          req.query.level as string,
         );
         return res.status(200).json({ categories });
+      } catch (e) {
+        logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    }
+  );
+
+  categoryRoute.get(
+    '/:id',
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger = Container.get('logger') as winston.Logger;
+      logger.debug('GET /category endpoint with params: %o', req.params);
+
+      try {
+        const categoryServiceInstance = Container.get(CategoryService);
+        const { category } = await categoryServiceInstance.get(
+          req.params.id as string
+        );
+        return res.status(200).json({ category });
       } catch (e) {
         logger.error('🔥 error: %o', e);
         return next(e);
--- server/src/services/category.ts
+++ server/src/services/category.ts
@@ -3,6 +3,9 @@ import { Model, Document } from 'mongoose';
 import winston from 'winston';
 import { ICategory } from '../interfaces/category';
 import { ICategoryModel } from '../models/category';
+import {
+  NotFoundError,
+} from '../modules/errors';
 
 @Service()
 export default class CategoryService {
@@ -16,11 +19,13 @@ export default class CategoryService {
 
   public async list(
       limit: string,
+      level: string,
   ): Promise<{ categories: ICategory[] }> {
     try {
       const take = parseInt(limit || '10', 10);
+      const categoryLevel = parseInt(level || '1', 10);
       const categoryRecords = await this.categoryModel
-        .find()
+        .find({ "value.categoryLevel": categoryLevel })
         .limit(take);
 
       const categories = categoryRecords.map((category) => category.toObject());
@@ -30,4 +35,18 @@ export default class CategoryService {
       throw e;
     }
   }
+
+  public async get(id: string): Promise<{ category: ICategory }> {
+      try {
+        const categoryRecord = await this.categoryModel.findOne({ _id: id });
+        if (!categoryRecord) {
+          throw new NotFoundError('Product is not exist');
+        }
+        const category = categoryRecord.toObject();
+        return { category };
+      } catch (e) {
+        this.logger.error(e);
+        throw e;
+      }
+    }
 }
