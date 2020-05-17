93913e7639eb0f271d56dbf66dc72781992fcd73
devsoopark <spark0128@dgu.ac.kr>
2020-05-17 23:51:58 +0900

    Add Loading Component

--- client/components/Common/InfinityList/styles.ts
+++ client/components/Common/InfinityList/styles.ts
@@ -10,6 +10,24 @@ const useStyles = makeStyles({
     width: 1300,
     margin: '30px 0px',
   },
+  loading: {
+    width: '100%',
+    maring: 10,
+  },
+  spinner: {
+    margin: '0 auto',
+    height: 40,
+    width: 40,
+    borderRadius: '50%',
+    border: '4px solid rgba(0, 0, 0, 0)',
+    borderTopColor: '#7396A5',
+    animation: '$spinner 600ms linear infinite',
+  },
+  '@keyframes spinner': {
+    '0%': {
+      transform: 'rotate(360deg)',
+    },
+  },
 });
 
 export default useStyles;
