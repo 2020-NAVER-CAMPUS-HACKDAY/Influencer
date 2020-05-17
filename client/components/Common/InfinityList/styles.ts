3ea90f350d9a70b27be7b3be9890b30c9a0989fb
devsoopark <spark0128@dgu.ac.kr>
2020-05-17 23:51:44 +0900

    Add InfinityList Component

---
+++ client/components/Common/InfinityList/styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  wrapper: {
+    width: 1300,
+    margin: '30px 0px',
+  },
+});
+
+export default useStyles;
