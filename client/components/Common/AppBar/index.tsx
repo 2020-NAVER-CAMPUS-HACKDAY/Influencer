82cb27e70066df672c050dc544c6a28c27efffed
jeongdaeun <daeun@squarelab.co>
2020-05-17 21:59:35 +0900

    Modify Global Components

--- client/components/Common/AppBar/index.tsx
+++ client/components/Common/AppBar/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import useStyles from 'components/Common/AppBar/styles';
 import MaterialAppBar from '@material-ui/core/AppBar';
+import clsx from 'clsx';
 
 export interface BarProps {
   isNotFixed?: boolean;
   backgroundColor?: string;
   backgroundImage?: string;
   height?: string;
+  rootClassName?: string;
+  containerClassName?: string;
 }
 
 const AppBar: React.FunctionComponent<BarProps> = (props) => {
   const classes = useStyles(props);
-  const { children } = props;
+  const { children, rootClassName, containerClassName } = props;
 
   return (
-    <MaterialAppBar className={classes.root}>
-      <div className={classes.container}>
+    <MaterialAppBar className={clsx(classes.root, rootClassName)}>
+      <div className={clsx(classes.container, containerClassName)}>
         {children}
       </div>
     </MaterialAppBar>
