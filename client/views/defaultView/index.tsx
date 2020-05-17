5dcc22667e994f3f116447545b30c25633c19e05
devsoopark <spark0128@dgu.ac.kr>
2020-05-17 23:55:56 +0900

    Add ProductListView Component

--- client/views/defaultView/index.tsx
+++ client/views/defaultView/index.tsx
@@ -1,42 +1,22 @@
 import React from 'react';
-import Link from 'next/link';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
   ProductProps,
   productActions,
   ProductActionsProps,
-  ProductItemProps,
 } from 'redux/ducks/product';
-import MainHeader from 'components/MainHeader';
-import { detailViewDataArray } from 'views/detailView/detailViewDummyData';
-import { PayloadAction } from 'typesafe-actions';
+import MainHeader from 'components/Main/MainHeader';
+import ProductListView from 'components/ProductListView';
 
 interface DefaultProps extends ProductProps, ProductActionsProps {
   data: string;
 }
 
 const Home: React.FC<DefaultProps> = (props) => {
-  const { data, addProduct } = props;
   return (
     <MainHeader>
-      <div onClick={
-        (): PayloadAction<
-        'product/ADD_PRODUCT',
-        ProductItemProps | ProductItemProps[]
-        > => addProduct(detailViewDataArray)
-      }>
-        This is Default Page.
-      </div>
-      <Link href="/interaction">
-        <a>go to Interaction Page</a>
-      </Link>
-      <Link href="/detail/[productID]" as="/detail/aaaa">
-        <a>
-        go to Next Page
-        </a>
-      </Link>
-      {data}
+      <ProductListView />
     </MainHeader>
   );
 };
@@ -47,5 +27,5 @@ export default connect<ProductProps, void>(
   }),
   (dispatch) => ({
     addProduct: bindActionCreators(productActions.addProduct, dispatch),
-  }),
+  })
 )(Home);
