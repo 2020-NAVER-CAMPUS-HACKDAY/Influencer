ddc2b6664f18aa1ba9106ed6f9f5ce8e7001df11
devsoopark <spark0128@dgu.ac.kr>
2020-05-17 23:47:15 +0900

    Add ProductItem Component

---
+++ client/components/ProductListView/ProductItem.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import useStyles from 'components/ProductListView/styles';
+
+interface ProductItemProps {
+  id: number;
+  price: number;
+  name: string;
+  category: number;
+  company: string;
+  imageUri: string;
+}
+
+const ProductItem: FC<ProductItemProps> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <article className={classes.card}>
+      <div className={classes.cardPhoto}>
+        <img className={classes.image} src={props.imageUri} />
+      </div>
+      <div className={classes.cardDesc}>
+        <h2 className={classes.name}>{props.name}</h2>
+        <div className={classes.company}>{props.company}</div>
+        <div className={classes.price}>{`${props.price}원`}</div>
+      </div>
+    </article>
+  );
+};
+
+export default ProductItem;
