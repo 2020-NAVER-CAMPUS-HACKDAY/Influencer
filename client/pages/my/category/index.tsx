ff7f07069eb222e9738af787cd6da5f96f14c216
jominjimail <jominjimail@gmail.com>
2020-05-18 20:09:46 +0900

    Add TODO list to me

--- client/pages/my/category/index.tsx
+++ client/pages/my/category/index.tsx
@@ -19,6 +19,7 @@ const MyCategory: React.FC<MyCategoryProps> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
+  // TODO(jominjimail): replace hard coding server address with .env parameter
   const res = await fetch('http://localhost:5000/api/categories?level=2');
   const { categories } = await res.json();
 
