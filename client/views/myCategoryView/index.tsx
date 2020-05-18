4fc96f8b6f77ff1c1747a063d0c864c55b6233c9
jominjimail <jominjimail@gmail.com>
2020-05-18 20:09:46 +0900

    Add TODO list to me

--- client/views/myCategoryView/index.tsx
+++ client/views/myCategoryView/index.tsx
@@ -7,6 +7,7 @@ interface MyCategoryViewProps {
   categoryData: Category[];
 }
 
+// TODO(jominjimail): data management with redux or hook
 const MyCategoryView: React.FC<MyCategoryViewProps> = (props) => {
   const [categories, setCategories] = useState<Category[]>([]);
   const { categoryData } = props;
@@ -19,6 +20,7 @@ const MyCategoryView: React.FC<MyCategoryViewProps> = (props) => {
   };
 
   const categoryDeleteHandler = (id: string): void => {
+    // TODO(jominjimail): remove this lint error
     // eslint-disable-next-line no-underscore-dangle
     const newCategories = categories.filter((category) => category._id !== id);
     setCategories(newCategories);
