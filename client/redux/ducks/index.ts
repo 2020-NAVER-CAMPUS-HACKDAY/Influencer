829824462c14264ca616fd6333b768ed9d02b108
jominjimail <jominjimail@gmail.com>
2020-05-20 01:01:05 +0900

    Add category redux setting

--- client/redux/ducks/index.ts
+++ client/redux/ducks/index.ts
@@ -1,17 +1,21 @@
 import { combineReducers } from 'redux';
 import { authReducer, UserProps, UserAction } from './auth';
 import { productReducer, ProductAction, ProductProps } from './product';
+import { categoryReducer, CategoryAction, CategoryProps } from './category';
 
 export const rootReducer = combineReducers({
   authReducer,
   productReducer,
+  categoryReducer,
 });
 
 export interface Types {
   authReducer: UserProps;
   productReducer: ProductProps;
+  categoryReducer: CategoryProps;
 }
 
 export type Actions =
   | UserAction
-  | ProductAction;
+  | ProductAction
+  | CategoryAction;
