42219661ab65c4e32834b1e10320cf24a1daaf42
jeongdaeun <daeun@squarelab.co>
2020-05-21 00:28:49 +0900

    Add product interfaces

--- server/src/interfaces/product.ts
+++ server/src/interfaces/product.ts
@@ -8,3 +8,26 @@ export interface IProduct {
 export interface IProductInputDTO {
   name: string;
 }
+
+export interface FetchProductForGridView extends IProduct {
+  modDate: Date;
+  productImages: Image[];
+  category: Category;
+}
+
+export interface ProductVerGridView {
+  productId: string;
+  imageLink: string;
+  category: string;
+  likeDate: Date;
+}
+
+export interface Image {
+  url: string;
+  width: string;
+  height: string;
+}
+
+export interface Category {
+  category1Id: string;
+}
