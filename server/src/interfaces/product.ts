b18dcdcf0daf10e054bce02d401751944ec1683e
Seogeurim <geulims@naver.com>
2020-05-21 18:16:25 +0900

    Add interface IProductforView

--- server/src/interfaces/product.ts
+++ server/src/interfaces/product.ts
@@ -1,10 +1,20 @@
 export interface IProduct {
-  productId: string;
-  productName: string;
+  _id: string;
+  category: Object;
+  productNo: Number;
+  name: String;
+  salePrice: Number;
   productImages: Object;
-  salePrice: number;
+  productInfoProvidedNoticeView: Object;
 }
 
 export interface IProductInputDTO {
   name: string;
+}
+
+export interface IProductforView {
+  productId: string;
+  productName: string;
+  productImages: Object;
+  salePrice: number;
 }
