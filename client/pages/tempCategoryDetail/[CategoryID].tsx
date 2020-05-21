9d914f575230b2d26bad7ff2c7a77620692fe1f9
jominjimail <jominjimail@gmail.com>
2020-05-22 03:43:03 +0900

    Add categoryChildrenData

--- client/pages/tempCategoryDetail/[CategoryID].tsx
+++ client/pages/tempCategoryDetail/[CategoryID].tsx
@@ -6,26 +6,39 @@ import { Category } from '../../interfaces/category';
 
 interface DetailCategoryProps extends CategoryProps {
   categoryData: Category;
+  categoryChildrenData: Category[];
 }
 
 const DetailCategory: FC<DetailCategoryProps> = (props) => {
-  const { categoryData } = props;
+  const { categoryData, categoryChildrenData } = props;
 
   return (
     <MainHeader>
       <div>카테고리 상세 페이지</div>
+      <div>{categoryData.value.wholeCategoryName}</div>
       <div>{categoryData.categoryId}</div>
+      <div>자식들</div>
+      {categoryChildrenData && categoryChildrenData.map((child) => (
+        <>
+          <div>{child.value.categoryName}</div>
+          <div>{child.categoryId}</div>
+        </>
+      ))}
     </MainHeader>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  // TODO(jominjimail): replace hard coding server address with .env parameter
   const res = await fetch(`http://localhost:5000/api/categories/${query.CategoryID}`);
   const { category } = await res.json();
+  const res2 = await fetch(`http://localhost:5000/api/categories/children/${query.CategoryID}`);
+  const { categories } = await res2.json();
 
   return {
     props: {
       categoryData: category,
+      categoryChildrenData: categories,
     },
   };
 };
