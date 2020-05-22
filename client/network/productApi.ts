af0dfc288275d11c5a6371708172490a03fb3690
jeongdaeun <daeun@squarelab.co>
2020-05-22 09:44:18 +0900

    Add LikeList API

--- client/network/productApi.ts
+++ client/network/productApi.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosResponse } from 'axios';
-import { ProductDataProps, ProductDetailProps } from 'redux/ducks/productInterface';
+import {
+  ProductDataProps, ProductDetailProps, LikeListProductProps,
+} from 'redux/ducks/Interface';
 import {
   PRODUCT_PAGE_API,
   PAGE_ADD,
   LIMIT_ADD,
   PRODUCT_LIMIT,
+  LIKE_LIST_API,
 } from 'constant';
 
 export const getProductDataArray = (pageId): Promise<AxiosResponse <(
@@ -12,8 +15,15 @@ Error
 | ProductDataProps[])>> => axios.get(
   `${process.env.SERVER_URL}${PRODUCT_PAGE_API}${PAGE_ADD}${pageId}${LIMIT_ADD}${PRODUCT_LIMIT}`,
 );
+
 export const getProductDataForProductId = (productId): Promise<AxiosResponse <(
 Error |
 ProductDetailProps)>> => axios.get(
   `${process.env.SERVER_URL}${PRODUCT_PAGE_API}${productId}`,
+);
+
+export const getLikeListData = (pageId): Promise<AxiosResponse <(
+Error |
+LikeListProductProps)>> => axios.get(
+  `${process.env.LOCAL_URL}${LIKE_LIST_API}${PAGE_ADD}${pageId}`,
 );
