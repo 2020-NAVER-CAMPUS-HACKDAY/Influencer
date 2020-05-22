169b4321cefefbb2565dc71085d11e81e66072aa
jominjimail <jominjimail@gmail.com>
2020-05-22 23:52:01 +0900

    Add routing when click the category name

--- client/components/CategoryHeader/WholeCategoryName/index.tsx
+++ client/components/CategoryHeader/WholeCategoryName/index.tsx
@@ -1,19 +1,29 @@
 import React, { FC, useState } from 'react';
 import useStyles from 'components/CategoryHeader/WholeCategoryName/styles';
+import Router from 'next/router';
 
 interface CategoryBoxProps {
-  data: string;
+  names: string;
+  ids: string;
 }
 const WholeCategoryName: FC<CategoryBoxProps> = (props) => {
-  const { data } = props;
+  const { names, ids } = props;
   const classes = useStyles();
-  const nameArray = data.split('>');
+  const nameArray = names.split('>');
+  const idArray = ids.split('>');
   const lastIndex = nameArray.length - 1;
+
+  const setCategory = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const categoryId: string = event.currentTarget.value;
+    Router.push(`/tempCategoryDetail/${categoryId}`);
+  };
 
   const makeElement = (name, index) => (
     (index === lastIndex)
-      ? <button className={classes.active}>{name}</button>
-      : <button className={classes.inactive}>{name}</button>
+      ? <button className={classes.active} onClick={setCategory} value={idArray[index]}>
+        {name}</button>
+      : <button className={classes.inactive} onClick={setCategory} value={idArray[index]}>
+        {name}</button>
   );
 
   return (
