cddd5d4e4d488bd332138d59f54d90028df524ca
ooeunz <yuns994@gmail.com>
2020-05-24 22:37:03 +0900

    create kafka prj

---
+++ kafka/src/main/java/com/consumer/kafka/Config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.consumer.kafka.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
