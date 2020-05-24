809bc17dc9ad2594e2b08e9b14bd0c6e6e0b49dc
ooeunz <yuns994@gmail.com>
2020-05-24 23:08:32 +0900

    set up kafka consumer

--- kafka/src/main/java/com/consumer/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/consumer/kafka/KafkaApplication.java
@@ -1,13 +1,69 @@
 package com.consumer.kafka;
 
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
 
 @SpringBootApplication
 public class KafkaApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(KafkaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+	private static Properties setConsumerProperty() {
+		Properties configs = new Properties();
+
+		configs.put("bootstrap.servers", "49.50.172.175:9092");
+		configs.put("group.id", "click_log_group");
+		configs.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		configs.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		configs.put("enable.auto.commit", "true");
+		configs.put("auto.commit.interval.ms", "1000");
+
+		configs.put("session.timeout.ms", "30000");
+
+		return configs;
+	}
+
+	public static void subscribe() throws Exception {
+		List<String> topicList = new ArrayList<>();
+		topicList.add("javainuse-topic");
+
+		KafkaConsumer<String, Object> consumer = new KafkaConsumer<String, Object>(setConsumerProperty());
+		consumer.subscribe(topicList);
+
+		try {
+			while (true) {
+				ConsumerRecords<String, Object> records = consumer.poll(500);
+
+				for (ConsumerRecord<String, Object> record : records) {
+					log.info("offset = " + record.offset() + "\tkey =" + record.key() + "\tvalue =" + record.value());
+
+					consumer.commitAsync(new OffsetCommitCallback() {
+						@Override
+						public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+						}
+					});
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				consumer.commitSync();
+			} finally {
+				consumer.close();
+			}
+		}
 	}
 
 }
