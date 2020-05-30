62a653e86f8249ef6ec7faa4ef7845bdee5d0a8b
jominjimail <jominjimail@gmail.com>
2020-05-30 23:59:07 +0900

    Edit fetch logic

--- client/network/categoryApi.ts
+++ client/network/categoryApi.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
-import { CategoryDataProps } from 'interfaces/category';
+import { CategoryDataProps, CategoryChildrenProps } from 'interfaces/category';
 
 export const getCategoryInfo = (categoryId: string | string[]): Promise<AxiosResponse <(
 Error
 | CategoryDataProps)>> => axios.get(
   `${process.env.SERVER_URL}categories/${categoryId}`,
+);
+
+export const getCategoryChildren = (categoryId: string | string[]): Promise<AxiosResponse <(
+Error
+| CategoryChildrenProps)>> => axios.get(
+  `${process.env.SERVER_URL}categories/children/${categoryId}`,
 );
--- client/views/searchCategoryView/index.tsx
+++ client/views/searchCategoryView/index.tsx
@@ -1,36 +1,73 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  FC, useEffect, useRef, useState,
+} from 'react';
 import MainHeader from 'components/Main/MainHeader';
 import { useRouter } from 'next/router';
 import { AxiosResponse } from 'axios';
-import { Category, CategoryDataProps } from 'interfaces/category';
-import { getCategoryInfo } from 'network/categoryApi';
+import { Category, CategoryDataProps, CategoryChildrenProps } from 'interfaces/category';
+import { getCategoryInfo, getCategoryChildren } from 'network/categoryApi';
 
 const SearchCategoryView: FC = (props) => {
-  const [categoryInfo, setCategoryInfo] = useState<Category>();
   const router = useRouter();
-  const { catId } = router.query;
+  /*
+  baseQuery -> undefined when catId is not lastLevel
+  baseQuery -> "" when catId is lastLevel
+   */
+  const { catId, baseQuery } = router.query;
+  const [categoryInfo, setCategoryInfo] = useState<Category>();
+  const [categoryChildren, setCategoryChildren] = useState<Category[]>([]);
 
   useEffect(() => {
-    // baseQuery is undefined when catId is not lastLevel
     if (catId === undefined) return;
+    console.log('here1');
     const fetch = async (): Promise<void> => {
       await getCategoryInfo(catId)
         .then(
           (response: AxiosResponse<CategoryDataProps>) => {
+            console.log(response.data.category);
             setCategoryInfo(response.data.category);
           },
         );
     };
+    const fetch2 = async (): Promise<void> => {
+      await getCategoryChildren(catId)
+        .then(
+          (response: AxiosResponse<CategoryChildrenProps>) => {
+            setCategoryChildren(response.data.categories);
+          },
+        );
+    };
     fetch();
+    if (baseQuery !== undefined) return;
+    fetch2();
   }, [catId]);
+
+  // useEffect(() => {
+  //   if (catId === undefined) return;
+  //   console.log(lastCategory.current);
+  //   if (lastCategory.current) return;
+  //   console.log('here2');
+  //   const fetch2 = async (): Promise<void> => {
+  //     await getCategoryChildren(catId)
+  //       .then(
+  //         (response: AxiosResponse<CategoryChildrenProps>) => {
+  //           setCategoryChildren(response.data.categories);
+  //         },
+  //       );
+  //   };
+  //   fetch2();
+  // }, [catId]);
 
   return (
     <MainHeader>
       <div>
         This is searchCategoryView Page
       </div>
-      {catId}
-      {categoryInfo.value.wholeCategoryId}
+      {baseQuery}
+      <div>현재</div>
+      {categoryInfo && categoryInfo.value.wholeCategoryId}
+      <div>자식</div>
+      {categoryChildren.map((child) => <div>{child.categoryId}</div>)}
     </MainHeader>
   );
 };
