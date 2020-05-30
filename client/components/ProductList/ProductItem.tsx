5c65c485d9ef4b4626baa5856f36f650fb89aa87
devsoopark <spark0128@dgu.ac.kr>
2020-05-30 16:00:05 +0900

    Apply addCommaStringFromTreeCntNum to product price

--- client/components/ProductList/ProductItem.tsx
+++ client/components/ProductList/ProductItem.tsx
@@ -3,13 +3,14 @@ import Router from 'next/router';
 import Heart from 'components/Common/Heart';
 import useStyles from 'components/ProductList/styles';
 import { Product } from 'interfaces/product';
+import { addCommaStringFromThreeCntNum } from 'utils/stringUtils';
 
 const ProductItem: FC<Product> = (props) => {
   const classes = useStyles();
 
   const cutName = (name: string): string => {
     const { length } = name;
-    return length > 17 ? `${name.split('').splice(0, 17).join('')} ...` : name;
+    return length > 24 ? `${name.split('').splice(0, 17).join('')} ...` : name;
   };
 
   const routeDetailPage = (): void => {
@@ -32,7 +33,9 @@ const ProductItem: FC<Product> = (props) => {
           <Heart />
         </div>
         <div className={classes.priceWrapper}>
-          <div className={classes.price}>{props.salePrice}</div>
+          <div className={classes.price}>
+            {addCommaStringFromThreeCntNum(props.salePrice)}
+          </div>
           <div className={classes.unit}>원</div>
         </div>
       </div>
