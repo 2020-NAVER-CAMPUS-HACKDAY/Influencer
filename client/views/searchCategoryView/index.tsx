1f110366dfda9b4e28aa3ac58ce2ac1136f711f1
jominjimail <jominjimail@gmail.com>
2020-05-30 15:21:20 +0900

    Add fetch logic

--- client/views/searchCategoryView/index.tsx
+++ client/views/searchCategoryView/index.tsx
@@ -1,19 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import MainHeader from 'components/Main/MainHeader';
 import { useRouter } from 'next/router';
+import { AxiosResponse } from 'axios';
+import { Category, CategoryDataProps } from 'interfaces/category';
+import { getCategoryInfo } from 'network/categoryApi';
 
-const Home: FC = (props) => {
+const SearchCategoryView: FC = (props) => {
+  const [categoryInfo, setCategoryInfo] = useState<Category>();
   const router = useRouter();
   const { catId } = router.query;
+
+  useEffect(() => {
+    // baseQuery is undefined when catId is not lastLevel
+    if (catId === undefined) return;
+    const fetch = async (): Promise<void> => {
+      await getCategoryInfo(catId)
+        .then(
+          (response: AxiosResponse<CategoryDataProps>) => {
+            setCategoryInfo(response.data.category);
+          },
+        );
+    };
+    fetch();
+  }, [catId]);
 
   return (
     <MainHeader>
       <div>
-        This is serachCategoryView Page
+        This is searchCategoryView Page
       </div>
       {catId}
+      {categoryInfo.value.wholeCategoryId}
     </MainHeader>
   );
 };
 
-export default Home;
+export default SearchCategoryView;
