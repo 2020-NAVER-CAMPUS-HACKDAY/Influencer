9c1e022f17fa1b992c1fc727b308df6de3528869
2016025987 <jominjimail@hanyang.ac.kr>
2020-06-03 23:19:51 +0900

    Remove tempMain

--- client/pages/my/index.tsx
+++ client/pages/my/index.tsx
@@ -1,1 +1,36 @@
-export { default } from 'views/myView';
+import React, { FC } from 'react';
+import MyHeader from 'components/Main/MyHeader';
+import { Category } from 'interfaces/category';
+import { GetStaticProps } from 'next';
+import { CATEGORY_API } from 'constant';
+import CategoryHeader from 'components/Category/Header';
+
+interface MyProps {
+  categoryData: Category[];
+}
+
+const My: FC<MyProps> = (props) => {
+  const { categoryData } = props;
+
+  return (
+    <>
+      <MyHeader />
+      <CategoryHeader
+        categoryData={categoryData}
+      />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(`${process.env.SERVER_URL}${CATEGORY_API}`);
+  const { categories } = await res.json();
+
+  return {
+    props: {
+      categoryData: categories,
+    },
+  };
+};
+
+export default My;
