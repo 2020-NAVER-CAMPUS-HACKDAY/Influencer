310f96b0d57ef80fd9b8a5e33515d7c6037525f8
2016025987 <jominjimail@hanyang.ac.kr>
2020-06-03 21:07:42 +0900

    Add /my pages

---
+++ client/views/myView/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import MainHeader from 'components/Main/MainHeader';
+
+const MyView: FC = () => (
+  <>
+    <MainHeader />
+    <div>마이 페이지 입니다</div>
+  </>
+);
+
+export default MyView;
