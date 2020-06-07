235240796d52d024c7f69d41db7cd54ae8927dfb
2016025987 <jominjimail@hanyang.ac.kr>
2020-06-07 15:48:32 +0900

    Remove goMy function in SVGButtom component

--- client/components/Common/SVGButton/index.tsx
+++ client/components/Common/SVGButton/index.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import clsx from 'clsx';
-import Router from 'next/router';
 import useStyles from './styles';
 
 interface SVGButtonProps {
@@ -12,14 +11,10 @@ const SVGButton: FC<SVGButtonProps> = (props) => {
   const classes = useStyles();
   const { className, handleClick, children } = props;
 
-  const goMy = (): void => {
-    Router.push('/my', undefined, { shallow: true });
-  };
-
   return (
     <button
       className={clsx(classes.button, className)}
-      onClick={handleClick ?? goMy}>
+      onClick={handleClick}>
       {children}
     </button>
   );
